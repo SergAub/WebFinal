b5c8819b5fea78f0633f75a478ea6e8d0b3851a0
SergAub <92084092+SergAub@users.noreply.github.com>
2025-02-02 22:43:57 +0700

    Удалил лишние файлы из кэша git

--- src/app/components/Footer.tsx
+++
@@ -1,7 +0,0 @@
-export default function Footer() {
-    return (
-        <footer className="bg-BlueBG p-8 shadow-up w-full max-w-7xl rounded-xl">
-            <p className="p">© 2024 ПР-21.102 Аюбов</p>
-        </footer>
-    );
-}
